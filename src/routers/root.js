fc7ca59a047e60989d982bbe5295334468c9ce76
Ritwik Neema <ritwikneema651@gmail.com>
2020-05-13 07:36:49 +0530

    Syntax error removed in root.js

--- src/routers/root.js
+++ src/routers/root.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = new express.Router();
 
 router.get('/', (req, res) => {
-    res.send(<h3>"Hi there! Please use the site from a API developement tool like Postman."</h3>); 
+    res.send("<h3>Hi there! Please use the site from a API developement tool like Postman.</h3>"); 
 });
 
 module.exports = router;
