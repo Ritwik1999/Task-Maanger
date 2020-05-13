546bea9bbe2e07e4aed62a3cd9bcab1c22abbc2d
Ritwik Neema <ritwikneema651@gmail.com>
2020-05-13 07:28:03 +0530

    Root page refactored

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ const express = require('express');
 require('./db/mongoose');   // for connection to database
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
+const rootRouter = require('./routers/root');
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());    //parses incoming JSON requests as JS objects.
+app.use(rootRouter);
 app.use(userRouter);
 app.use(taskRouter); 
-
-app.get('/', (req, res) => {
-   res.send(<h3>"Hi there! Please use the site from a API developement tool like Postman."</h3>); 
-});
 
 app.listen(port, () => {
     console.log('Server is up on the port ' + port);
---
+++ src/routers/root.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = new express.Router();
+
+router.get('/', (req, res) => {
+    res.send(<h3>"Hi there! Please use the site from a API developement tool like Postman."</h3>); 
+});
+
+module.exports = router;
